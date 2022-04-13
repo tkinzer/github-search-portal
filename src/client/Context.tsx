5f870d1b1327b2fa615c84eea7eb92d8b30ea0b0
Tim <timothykinzer@gmail.com>
2022-04-13 11:22:32 -0700

    Add search history and search button

--- src/client/Context.tsx
+++ src/client/Context.tsx
@@ -3,10 +3,12 @@ import React, { useContext, useState } from "react";
 export interface Context {
   searchValue: string;
   setSearchValue: (val: string) => void;
+  searchHistory: string[];
 }
 const defaultVal = {
   searchValue: "",
   setSearchValue: () => {},
+  searchHistory: [],
 } as Context;
 
 const context = React.createContext(defaultVal);
@@ -15,8 +17,26 @@ const { Provider } = context;
 
 export const ContextWrapper = ({ children }: { children: any }) => {
   const [searchValue, setSearchValue] = useState(defaultVal.searchValue);
+  const [searchHistory, setSearchHistory] = useState<string[]>([]);
+
+  React.useEffect(() => {
+    if (!searchHistory) {
+      // TODO check if searchHistory is empty
+      const localSearchHistory = localStorage.getItem("searchHistory");
+      if (localSearchHistory) {
+        setSearchHistory(JSON.parse(localSearchHistory));
+      }
+    }
+
+    const updatedSearchHistory = [...searchHistory, searchValue];
+
+    setSearchHistory(updatedSearchHistory);
+  }, [searchValue]);
+
   return (
-    <Provider value={{ searchValue, setSearchValue }}>{children}</Provider>
+    <Provider value={{ searchValue, setSearchValue, searchHistory }}>
+      {children}
+    </Provider>
   );
 };
 
--- src/client/components/Search/SearchBar.tsx
+++ src/client/components/Search/SearchBar.tsx
@@ -2,21 +2,32 @@ import React from "react";
 import { useAppContext } from "../../Context";
 
 export const SearchBar = (): JSX.Element => {
-  const { searchValue, setSearchValue } = useAppContext();
+  const { searchValue, setSearchValue, searchHistory } = useAppContext();
+  const searchRef = React.useRef<HTMLInputElement>(null);
 
-  const updateSearchValue = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.debug("updateSearchValue", e.target.value);
-    setSearchValue(e.target.value);
+  const updateSearchValue = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.debug("updateSearchValue", searchRef.current?.value);
+    setSearchValue(searchRef.current?.value ?? "");
   };
 
   return (
     <div className="p-2">
+      {searchHistory.length > 0 && (
+        <div className="flex flex-row">{searchHistory.toString()}</div>
+      )}
       <input
+        ref={searchRef}
+        defaultValue={searchValue}
         placeholder={"Search repos"}
-        onChange={updateSearchValue}
         style={{ background: "#8080802e" }}
         className="focus:ring-indigo-500 focus:border-indigo-500 block w-fit text-2xl border-gray-300 rounded-md p-2"
       />
+      <button
+        onClick={(e) => updateSearchValue(e)}
+        className="bg-gray-300 text-gray-800 rounded-md p-2"
+      >
+        Search
+      </button>
     </div>
   );
 };
